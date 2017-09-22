dc0b93096049d3923bb03fa73c41db3fc1ba7916
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-09-22 13:25:14 +0200

    Cross-build for Windows

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd ffmpeg-3.3.4
-./configure
+./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --disable-static --enable-shared --disable-programs --disable-doc --disable-avdevice --disable-network
 make
