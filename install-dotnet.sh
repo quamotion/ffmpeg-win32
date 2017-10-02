38845499b465f8525454afa44a7673dd21e81899
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-10-02 22:17:44 +0200

    Remove verbose flag

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -1,4 +1,4 @@
 wget -nv -nc https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-linux-x64.tar.gz -O ~/dotnet-sdk-2.0.0-linux-x64.tar.gz
 mkdir -p ~/dotnet
-tar xvzf ~/dotnet-sdk-2.0.0-linux-x64.tar.gz -C ~/dotnet
+tar xzf ~/dotnet-sdk-2.0.0-linux-x64.tar.gz -C ~/dotnet
 ~/dotnet/dotnet --version
