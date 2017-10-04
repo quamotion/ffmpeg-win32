e42479460c23ff2e8382c1af9c0607883b1d3510
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-10-04 11:27:03 +0200

    Enable the file protocol
    Useful for unit tests.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd ffmpeg-3.3.4
-./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --disable-static --enable-shared --enable-cuda --enable-cuvid --enable-dxva2 --disable-programs --disable-doc --disable-avdevice --disable-swresample --disable-postproc --disable-avfilter --disable-postproc --disable-avfilter --disable-network --disable-everything --enable-decoder=h264 --enable-decoder=h264_cuvid --enable-hwaccel=h264_cuvid --enable-hwaccel=h264_dxva2 --enable-parser=h264 --enable-demuxer=h264
+./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --disable-static --enable-shared --enable-cuda --enable-cuvid --enable-dxva2 --disable-programs --disable-doc --disable-avdevice --disable-swresample --disable-postproc --disable-avfilter --disable-postproc --disable-avfilter --disable-network --disable-everything --enable-decoder=h264 --enable-decoder=h264_cuvid --enable-hwaccel=h264_cuvid --enable-hwaccel=h264_dxva2 --enable-parser=h264 --enable-demuxer=h264 --enable-protocol=file
 make
 cd ..
