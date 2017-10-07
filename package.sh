78030c0652eaab881566385cd752197b06b494d1
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-10-07 21:47:16 +0200

    Fix dotnet restore command

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
-~/dotnet/dotnet restore --version-suffix r$TRAVIS_BUILD_NUMBER
+~/dotnet/dotnet restore /p:VersionSuffix=r%APPVEYOR_BUILD_NUMBER%
 ~/dotnet/dotnet pack -c Release --version-suffix r$TRAVIS_BUILD_NUMBER
