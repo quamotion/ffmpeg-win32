f516ef01723e3a7f8dd30cd7035447ade967ab93
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-10-16 10:24:27 +0200

    Fix library search paths for OS X and Linux

---
+++ patch-libraries-linux.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+out="$HOME/out/"
+ffmpeg="$HOME/ffmpeg-3.3.4/"
+kvazaar="$HOME/kvazaar-1.1.0/"
+
+mkdir -p $out
+
+cp $ffmpeg/libavcodec/libavcodec.so.57 $out
+cp $ffmpeg/libavformat/libavformat.so.57 $out
+cp $ffmpeg/libavutil/libavutil.so.55 $out
+cp $ffmpeg/libswscale/libswscale.so.4 $out
+cp $kvazaar/src/.libs/libkvazaar.so.3 $out
+
+# For more info, see:
+# https://github.com/NixOS/patchelf
+# http://man7.org/linux/man-pages/man8/ld.so.8.html
+
+patchelf=patchelf-0.9/src/patchelf
+
+for f in $out/*; do
+   chmod +w $f
+
+   $patchelf --set-rpath '${ORIGIN}' $f
+
+   readelf -d $f
+done
---
+++ patch-libraries-osx.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+out="$HOME/out/"
+ffmpeg="$HOME/ffmpeg-3.3.4/"
+kvazaar="$HOME/kvazaar-1.1.0/"
+
+mkdir -p $out
+
+cp $ffmpeg/libavcodec/libavcodec.57.dylib $out
+cp $ffmpeg/libavformat/libavformat.57.dylib $out
+cp $ffmpeg/libavutil/libavutil.55.dylib $out
+cp $ffmpeg/libswscale/libswscale.4.dylib $out
+cp $kvazaar/src/.libs/libkvazaar.3.dylib $out
+
+# Patch the dylib dependencies for all .dylib files in the out directory
+for f in "$out/"*.dylib; do
+   chmod +w "$f"
+
+   # Skip the first two lines of the otool output, this is just the name of the dylib itself
+   dylibs=`otool -L "$f" | tail -n +3 | grep "/Users/travis/build/" | awk -F' ' '{ print $1 }'`
+
+   for dylib in $dylibs; do
+     basename=`basename "$dylib"`
+
+     if [ ! -f "$out/lib/$basename" ]; then
+        echo "error: The dylib file '$out/$basename' does not exist in the output folder; referenced from $f"
+     fi
+
+     # https://www.mikeash.com/pyblog/friday-qa-2009-11-06-linking-and-install-names.html
+     install_name_tool -change "$dylib" "@loader_path/$basename" "$f"
+   done;
+
+   otool -L "$f"
+done
