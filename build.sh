943243eeb8cc2303b81e40de291f1b7d914786e5
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-03-12 09:50:33 +0100

    Prototype Azure pipelines

--- build.sh
+++
@@ -1,7 +0,0 @@
-cd ffmpeg-3.3.4
-
-# See https://github.com/FFmpeg/FFmpeg/blob/master/configure for an overview of all configuration
-# options
-./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --disable-static --enable-shared --enable-version3 --enable-cuda --enable-cuvid --enable-dxva2
-make
-cd ..
